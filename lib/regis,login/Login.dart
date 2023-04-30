cad861dfd12dca2c3bb28dc2c8937222647da2ab
big555ddd <biggafu5@hotmail.com>
2023-04-30 21:29:37 +0700

    login regis

--- lib/regis,login/Login.dart
+++ lib/regis,login/Login.dart
@@ -39,6 +39,18 @@ class _LoginState extends State<Login> {
   final _auth = FirebaseAuth.instance;
   final dbRef =
       FirebaseFirestore.instance.collection('users').doc('admin').get();
+  final textFieldFocusNode = FocusNode();
+  bool _obscured = true;
+
+  void _toggleObscured() {
+    setState(() {
+      _obscured = !_obscured;
+      if (textFieldFocusNode.hasPrimaryFocus)
+        return; // If focus is on text field, dont unfocus
+      textFieldFocusNode.canRequestFocus =
+          false; // Prevents focus if tap on eye
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -135,12 +147,24 @@ class _LoginState extends State<Login> {
                           width: 360,
                           child: TextFormField(
                             controller: passwordController,
-                            obscureText: true,
+                            obscureText: _obscured,
                             decoration: textInputDecoration.copyWith(
                               hintText: 'Password',
                               prefixIcon: const Icon(
                                 Icons.lock,
                                 color: green,
+                              ),
+                              suffixIcon: Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 0, 4, 0),
+                                child: GestureDetector(
+                                  onTap: _toggleObscured,
+                                  child: Icon(
+                                    _obscured
+                                        ? Icons.visibility_rounded
+                                        : Icons.visibility_off_rounded,
+                                    size: 24,
+                                  ),
+                                ),
                               ),
                             ),
                             onChanged: (val) {
--- lib/regis,login/Register.dart
+++ lib/regis,login/Register.dart
@@ -30,6 +30,29 @@ class _RegistrationScreen extends State<RegisterPage> {
   bool verify = false;
   bool isadmin = false;
   final textEditingController = TextEditingController();
+  final textFieldFocusNode = FocusNode();
+  bool _obscured = true;
+  bool _obscured1 = true;
+
+  void _toggleObscured() {
+    setState(() {
+      _obscured = !_obscured;
+      if (textFieldFocusNode.hasPrimaryFocus)
+        return; // If focus is on text field, dont unfocus
+      textFieldFocusNode.canRequestFocus =
+          false; // Prevents focus if tap on eye
+    });
+  }
+
+  void _toggleObscured1() {
+    setState(() {
+      _obscured1 = !_obscured1;
+      if (textFieldFocusNode.hasPrimaryFocus)
+        return; // If focus is on text field, dont unfocus
+      textFieldFocusNode.canRequestFocus =
+          false; // Prevents focus if tap on eye
+    });
+  }
 
   @override
   void initState() {
@@ -145,13 +168,27 @@ class _RegistrationScreen extends State<RegisterPage> {
                                 alignment: Alignment.center,
                                 width: 360,
                                 child: TextFormField(
-                                  obscureText: true,
+                                  obscureText: _obscured,
                                   decoration: textInputDecorationp.copyWith(
-                                      hintText: "Password",
-                                      prefixIcon: Icon(
-                                        Icons.lock,
-                                        color: Theme.of(context).primaryColor,
-                                      )),
+                                    hintText: "Password",
+                                    prefixIcon: Icon(
+                                      Icons.lock,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                    suffixIcon: Padding(
+                                      padding:
+                                          const EdgeInsets.fromLTRB(0, 0, 4, 0),
+                                      child: GestureDetector(
+                                        onTap: _toggleObscured,
+                                        child: Icon(
+                                          _obscured
+                                              ? Icons.visibility_rounded
+                                              : Icons.visibility_off_rounded,
+                                          size: 24,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
                                   validator: (val) {
                                     if (val!.length < 6) {
                                       return "Password must be at least 6 characters";
@@ -173,13 +210,27 @@ class _RegistrationScreen extends State<RegisterPage> {
                                 alignment: Alignment.center,
                                 width: 360,
                                 child: TextFormField(
-                                  obscureText: true,
+                                  obscureText: _obscured1,
                                   decoration: textInputDecorationp.copyWith(
-                                      hintText: "Confirm Password",
-                                      prefixIcon: Icon(
-                                        Icons.lock,
-                                        color: Theme.of(context).primaryColor,
-                                      )),
+                                    hintText: "Confirm Password",
+                                    prefixIcon: Icon(
+                                      Icons.lock,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                    suffixIcon: Padding(
+                                      padding:
+                                          const EdgeInsets.fromLTRB(0, 0, 4, 0),
+                                      child: GestureDetector(
+                                        onTap: _toggleObscured1,
+                                        child: Icon(
+                                          _obscured
+                                              ? Icons.visibility_rounded
+                                              : Icons.visibility_off_rounded,
+                                          size: 24,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
                                   validator: (val) {
                                     if (password == password2) {
                                       return null;
